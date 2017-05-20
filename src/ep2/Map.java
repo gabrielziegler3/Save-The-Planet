f80656e5992975ac9cd06806207c99f2f28c39ca
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-05-20 19:50:04 -0300

    Added status header and Alien to map

--- src/ep2/Map.java
+++ src/ep2/Map.java
@@ -16,29 +16,38 @@ import javax.swing.Timer;
 
 public class Map extends JPanel implements ActionListener {
 
-    private final int SPACESHIP_X = 220;
-    private final int SPACESHIP_Y = 430;
+    private final int SPACESHIP_X = 700;
+    private final int SPACESHIP_Y = 750;
+    private final int ALIEN_X = 700;
+    private final int ALIEN_Y = 100;
+//    private final int LASER_X = ;
+//    private final int LASER_Y =   
     private final Timer timer_map;
     
     private final Image background;
     private final Spaceship spaceship;
+    private final Alien alien;
+    private final Game game;
+//    private final LaserBeam laser;
 
     public Map() {
         
-        addKeyListener(new KeyListerner());
+        addKeyListener(new KeyListener());
         
         setFocusable(true);
         setDoubleBuffered(true);
-        ImageIcon image = new ImageIcon("images/space.jpg");
+        game = new Game(1);
+        ImageIcon space = new ImageIcon("images/space" + game.getStage() + ".jpg");
         
-        this.background = image.getImage();
-
+        this.background = space.getImage();
         spaceship = new Spaceship(SPACESHIP_X, SPACESHIP_Y);
+        alien  = new Alien(ALIEN_X ,ALIEN_Y, 1);
 
         timer_map = new Timer(Game.getDelay(), this);
         timer_map.start();
-                            
+         
     }
+    
     
     @Override
     public void paintComponent(Graphics g) {
@@ -46,26 +55,36 @@ public class Map extends JPanel implements ActionListener {
         
         g.drawImage(this.background, 0, 0, null);
        
-        draw(g);
+        drawSpaceship(g);
+        drawAlien(g);
+        drawStatus(g, game, spaceship);
+//        drawLaser(g);
 
         Toolkit.getDefaultToolkit().sync();
     }
 
-    private void draw(Graphics g) {
-               
+    private void drawSpaceship(Graphics g) {
         // Draw spaceship
         g.drawImage(spaceship.getImage(), spaceship.getPositionX(), spaceship.getPositionY(), this);
     }
+    private void drawAlien(Graphics g){
+        g.drawImage(alien.getImage(), alien.getPositionX(),alien.getPositionY(), this);
+    }
+//    private void drawLaser(Graphics g){
+//        g.drawImage(laser.getImage(), laser.getPositionX(), laser.getPositionY(), this);
+//    }
     
     @Override
     public void actionPerformed(ActionEvent e) {
        
         updateSpaceship();
+        updateAlien();
+//        updateLaserBeam();
        
         repaint();
     }
     
-    private void dranMissionAccomplished(Graphics g) {
+    private void drawMissionAccomplished(Graphics g) {
 
         String message = "MISSION ACCOMPLISHED";
         Font font = new Font("Helvetica", Font.BOLD, 14);
@@ -74,6 +93,20 @@ public class Map extends JPanel implements ActionListener {
         g.setColor(Color.white);
         g.setFont(font);
         g.drawString(message, (Game.getWidth() - metric.stringWidth(message)) / 2, Game.getHeight() / 2);
+    }
+    
+    private void drawStatus(Graphics g, Game game, Spaceship spaceship){
+        String stage = "Stage: " + game.getStage();
+        String life = "Life: " + spaceship.getLife();
+        String score = "Score: " + spaceship.getScore();
+        Font font = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metric = getFontMetrics(font);
+
+        g.setColor(Color.white);
+        g.setFont(font);
+        g.drawString(stage, (Game.getWidth() - metric.stringWidth(stage)) - 1650, Game.getHeight() - 950);
+        g.drawString(life, (Game.getWidth() - metric.stringWidth(stage)) - 1650, Game.getHeight() - 935);
+        g.drawString(score, (Game.getWidth() - metric.stringWidth(stage)) -1650, Game.getHeight() - 920);
     }
     
     private void drawGameOver(Graphics g) {
@@ -91,8 +124,17 @@ public class Map extends JPanel implements ActionListener {
         spaceship.move();
     }
   
+    private void updateAlien() {
+        alien.move();
+//        System.out.println("Laserbeam X = " + laser.getPositionX() + "Laserbeam Y" +laser.getPositionY());
+    }
+    
+//    private void updateLaserBeam() {
+//        laser.move();
+//    }
+//    
 
-    private class KeyListerner extends KeyAdapter {
+    private class KeyListener extends KeyAdapter {
         
         @Override
         public void keyPressed(KeyEvent e) {
