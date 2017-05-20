c3e1b4ae0d1081d5ba2d9483c16157d307730861
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-05-20 19:46:21 -0300

    Updated alien movement and sprite

--- src/ep2/Alien.java
+++ src/ep2/Alien.java
@@ -5,13 +5,36 @@ package ep2;
  * @author ziegler
  */
 
+public class Alien extends GameObject{
 
-public class Alien extends Sprite{
+    private int speedX = 3;
+    private int speedY = 1;
+    private int type;
 
-    private int speedY;
-
-    public Alien(int posx, int posy) {
+    public Alien(int posx, int posy, int type) {
         super(posx, posy);
+        this.type = type;
+        initAlien();
+    }
+    
+    private void initAlien(){
+        loadAlien();
+    }
+    
+    public void loadAlien(){
+        switch (type) {
+            case 1:
+                loadImage("images/crab-moving-transparent.gif");
+                break;
+            case 2:
+                loadImage("images/crab-moving-transparent.gif");
+                break;
+            case 3:
+                loadImage("images/crab-moving-transparent.gif");
+                break;
+            default:
+                break;
+        }
     }
 
     public int getSpeedY() {
@@ -20,6 +43,39 @@ public class Alien extends Sprite{
 
     public void setSpeedY(int speedY) {
         this.speedY = speedY;
+    }
+      public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+    
+     public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    
+    public void move(){
+    // Limits the movement of the spaceship to the side edges.
+        if((speedX < 0 && positionX <= 0) || (speedX > 0 && positionX + width >= Game.getWidth())){
+            speedX *= -1;
+            speedY += 50;
+            positionY += speedY;
+        }
+        
+        // Moves the spaceship on the horizontal axis
+        positionX += speedX;
+        
+        // Limits the movement of the spaceship to the vertical edges.
+        if((speedY < 0 && positionY <= 0) || (speedY > 0 && positionY + height >= Game.getHeight())){
+            speedY = 0;
+        }
     }
     
 }
