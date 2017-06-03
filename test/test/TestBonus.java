c294fbf9f43ea6725b69e710ef4bbdc327622fed
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-06-03 13:48:28 -0300

    Added test class for Bonus

--- test/test/TestBonus.java
+++ test/test/TestBonus.java
@@ -1,9 +1,36 @@
 package test;
 
+import ep2.Bonus;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  *
  * @author ziegler
  */
-public class TestBonus {
+public class TestBonus {    
+    Bonus bonus;
     
+    @Before
+    public void setUp() {
+        bonus = new Bonus(400, 400, 1, 1);
+    }
+    
+    @Test
+    public void testStationaryBonus(){
+        bonus.setSpeedX(0);
+        bonus.setSpeedY(0);
+        
+        assertEquals(0, bonus.getSpeedY());
+        assertEquals(0, bonus.getSpeedX());
+    }
+    
+    @Test 
+    public void testBonusMovement() {
+        bonus.move();
+        
+        assertEquals(400, bonus.getPositionX());
+        assertEquals(401, bonus.getPositionY());
+    }
 }
